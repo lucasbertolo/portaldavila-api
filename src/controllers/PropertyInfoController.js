55f472bebf737440eac75de5811e25696b91e68f
Lucas <lucasbertolo2@gmail.com>
2019-10-05 20:19:29 -0300

    Adding methods to PropInfoService

--- src/controllers/PropertyInfoController.js
+++ src/controllers/PropertyInfoController.js
@@ -24,110 +24,37 @@ const GetAll = (req, res, db) => {
     .catch(() => res.status(400).json('No available houses'));
 };
 
-// Retorno de imovel por id
-// Verificar se id existe
-// Tratamento erro caso id nao exista
-// Buscar informações complementares:
-// Fotos
-// Disponibilidade visita
-// Tratamento caso fotos vazias
-// Retorno Json com objeto com imovel
+
 const Get = (req, res, db) => {
-  const { id } = req.params;
-  db.select('*').from('property').where({ id })
-    .then((usr) => {
-      if (usr.length) {
-        res.json(usr[0]);
-      } else {
-        res.status(400).json('Property not found');
-      }
-    })
+  PropertyInfoService.Get(req, db)
+    .then((resp) => res.json(resp))
     .catch((err) => res.status(400).json(`Error getting Property - ${err}`));
 };
 
-// Retorno de imovel por filtros
-// Verificar tipo de filtro
-// Metodo especifico para cada filtro - Criar metodo externo
-// Retornar json com array de imoveis
-
-// Inserção de imovelc
-// Verificação dos dados necessarios inseridos
-// Tratamento caso falte dados
-// Chamar photoController para inserir imagem em servidor externo
-// Salvar id retornado na tabela de photos
 const Add = (req, res, db) => {
-  // const {
-  //   neighborhood_id,
-  //   position,
-  //   price,
-  //   purpose_id,
-  //   type_id,
-  //   creator_id,
-  //   area,
-  //   building_area,
-  // } = req.body.data.info;
-
+  // Alterar dps para o metodo receber certo o valor
   const data = req.body.data.info;
 
   PropertyInfoService.Add(db, data)
     .then(() => res.status(200).json('New property registered'))
     .catch((err) => res.status(400).json(`Error - ${err}`));
-  // Creator ID - pegar sempre do usuario fazendo a atualizacao
-  // Checar se ele é consultor
-  // db('property')
-  //   .insert({
-  //     neighborhood_id,
-  //     position,
-  //     price,
-  //     area,
-  //     building_area,
-  //     purpose_id,
-  //     type_id,
-  //     creator_id,
-  //   })
-  //   .then(() => res.status(200).json('New property registered'))
-  //   .catch((err) => res.status(400).json(`Error - ${err}`));
 };
 
 
-// Atualização de imovel -> Controller Photos
-// Tratamento erro caso id nao exista
-// Merge dados e update entidade
 const Update = (req, res, db) => {
-  const { id } = req.params;
-
-  db('property')
-    .where({ id })
-    .update({
-      ...req.body,
-    })
-    .then((data) => {
-      if (data === 1) {
-        res.status(200).json('Property updated');
-      } else res.status(400).json('Id inexistent');
-    })
-    .catch((err) => res.status(400).json(`erro - ${err}`));
-
-  // Pegar id automatico do usuario que esta mexendo
+  PropertyInfoService.Update(req, db)
+    .then((item) => res.status(200).json(item))
+    .catch((err) => res.status(400).json(err));
 };
 
 
-// Delete Imovel -> Controller Photos
 const Remove = (req, res, db) => {
-  const { id } = req.params;
-
   // Checar por token ou validar autorizacao para apagar usuario
-  // Delete no banco e tabelas relacionadas
-  // Deletar fotos na AWS
-  db('property')
-    .where({ id })
-    .del()
-    .then((data) => {
-      if (data === 1) {
-        res.status(200).json('Property deleted');
-      } else res.status(400).json('Id inexistent');
-    })
-    .catch((err) => res.status(400).json(`Error - ${err}`));
+  // Delete no banco e tabelas relacionadas - CASCADE
+  // Deletar fotos na AWS - AUTOMATIZAR
+  PropertyInfoService.Remove(req, db)
+    .then((item) => res.status(200).json(item))
+    .catch((err) => res.status(400).json(err));
 };
 
 const GetDetails = (req, res, db) => {
@@ -141,7 +68,7 @@ const GetDetails = (req, res, db) => {
       if (data.length) {
         res.json(data[0]);
       } else {
-        res.status(400).json('Propriedade inexistente');
+        res.status(400).json('Property not found');
       }
     })
     .catch((err) => {
--- src/services/PropertyInfoService.js
+++ src/services/PropertyInfoService.js
@@ -9,10 +9,59 @@ const Add = (db, data) => {
     })
     .then((res) => res)
     .catch((err) => {
-      Promise.reject(new Error(`Error - ${err}`));
+      Promise.reject(new Error(err));
     });
+};
+
+const Get = (req, db) => {
+  const { id } = req.params;
+
+  return db.select('*').from('property').where({ id })
+    .then((usr) => {
+      if (usr.length) {
+        return usr[0];
+      }
+      return Promise.reject(new Error('Property not found'));
+    })
+    .catch((err) => Promise.reject(new Error(err)));
+};
+
+const Update = (req, db) => {
+  const { id } = req.params;
+
+  return db('property')
+    .where({ id })
+    .update({
+      ...req.body,
+    })
+    .then((data) => {
+      if (data === 1) {
+        return 'Property updated';
+      } return Promise.reject(new Error('Property not found'));
+    })
+    .catch((err) => Promise.reject(new Error(err)));
+};
+
+const Remove = (req, db) => {
+  const { id } = req.params;
+
+  // Checar por token ou validar autorizacao para apagar usuario
+  // Delete no banco e tabelas relacionadas - CASCADE
+  // Deletar fotos na AWS - AUTOMATIZAR
+  return db('property')
+    .where({ id })
+    .del()
+    .then((data) => {
+      if (data === 1) {
+        return 'Property deleted';
+      } return Promise.reject(Error('Id inexistent'));
+    })
+    .catch((err) => Promise.reject(new Error(err)));
 };
 
 module.exports = {
   Add,
+  Get,
+  Update,
+  Remove,
 };
