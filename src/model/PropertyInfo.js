4f2679ccfb6f3d2c3218ab88d4701b6d5760b851
Lucas <lucasbertolo2@gmail.com>
2019-10-17 00:13:03 -0300

    validating position

--- src/model/PropertyInfo.js
+++ src/model/PropertyInfo.js
@@ -1,7 +1,7 @@
 /* eslint-disable camelcase */
 function PropertyInfo(data) {
   this.neighborhood_id = data.neighborhood_id || 0;
-  this.position = data.position;
+  this.position = data.position ? JSON.stringify(data.position) : null;
   this.price = data.price || 0;
   this.purpose_id = data.purpose_id || 0;
   this.type_id = data.type_id || 0;
