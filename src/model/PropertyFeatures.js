593abdcce963bfc83ea309cf3c8e4b43d67ad964
Lucas <lucasbertolo2@gmail.com>
2019-11-10 19:06:12 -0300

    camelcase features

--- src/model/PropertyFeatures.js
+++ src/model/PropertyFeatures.js
@@ -1,15 +1,15 @@
 /* eslint-disable camelcase */
 function PropertyFeatures(data, id) {
   this.description = data.description;
-  this.air_conditioning = data.air_conditioning || 0;
+  this.air_conditioning = data.airCondtioner || 0;
   this.pool = data.pool || 0;
   this.balcony = data.balcony || 0;
-  this.barbecue_grill = data.barbecue_grill || 0;
+  this.barbecue_grill = data.barbecueGrill || 0;
   this.stairway = data.stairway || 0;
   this.garden = data.garden || 0;
   this.property_id = id || 0;
-  this.fire_security = data.fire_security || 0;
-  this.camera_security = data.camera_security || 0;
+  this.fire_security = data.fireSecurity || 0;
+  this.camera_security = data.cameraSecurity || 0;
 
   return (
     this.description,
