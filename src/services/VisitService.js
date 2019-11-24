8f0bce12972e4cfeefc4fe8542f52c2e59176ab5
Lucas <lucasbertolo2@gmail.com>
2019-11-24 15:33:22 -0300

    checking entries in visit

--- src/services/VisitService.js
+++ src/services/VisitService.js
@@ -5,41 +5,42 @@ const Add = (db, data) => new Promise((resolve, reject) => db('visit_schedule')
   .then((res) => resolve(res))
   .catch((err) => reject(Error(err))));
 
-const Update = (db, id, data) => new Promise((resolve, reject) => {
+const Update = (db, id, data) => new Promise((resolve, reject) => db('visit_schedule')
+  .update({
+    ...data,
+  })
+  .where({ id })
+  .then((res) => resolve(res))
+  .catch((err) => reject(Error(err))));
+
+const Get = (db, user_id) => new Promise((resolve, reject) => db('visit_schedule As vs')
+  .join('property As p', 'p.id', 'vs.property_id')
+  .join('property_photos As pp', 'pp.property_id', 'p.id')
+  .join('user As u', 'u.id', 'vs.user_id')
+  .select('p.*', 'u.*', 'pp.cdn As url', 'vs.id As visit_id', 'time_register', 'date_register', 'status', 'vs.property_id')
+  .where({ user_id })
+  .andWhere('pp.iscover', 'true')
+  .then((res) => resolve(res))
+  .catch((err) => reject(Error(err))));
+
+const GetAll = (db) => new Promise((resolve, reject) => db('visit_schedule As vs')
+  .join('property As p', 'p.id', 'vs.property_id')
+  .join('user As u', 'u.id', 'vs.user_id')
+  .select('p.*', 'u.*', 'vs.id AS visit_id', 'time_register', 'date_register', 'status', 'vs.property_id')
+  .then((res) => resolve(res))
+  .catch((err) => reject(Error(err))));
+
+const Remove = (db, property_id) => new Promise((resolve, reject) => db('visit_schedule')
+  .where({ property_id })
+  .del()
+  .then((res) => resolve(res))
+  .catch((err) => reject(Error(err))));
+
+const checkEntries = (db, data) => new Promise((resolve, reject) => {
+  const { user_id, property_id } = data;
   db('visit_schedule')
-    .update({
-      ...data,
-    })
-    .where({ id })
-    .then((res) => resolve(res))
-    .catch((err) => reject(Error(err)));
-});
-
-const Get = (db, user_id) => new Promise((resolve, reject) => {
-  db('visit_schedule As vs')
-    .join('property As p', 'p.id', 'vs.property_id')
-    .join('property_photos As pp', 'pp.property_id', 'p.id')
-    .join('user As u', 'u.id', 'vs.user_id')
-    .select('p.*', 'u.*', 'pp.cdn As url', 'vs.id As visit_id', 'time_register', 'date_register', 'status', 'vs.property_id')
-    .where({ user_id })
-    .andWhere('pp.iscover', 'true')
-    .then((res) => resolve(res))
-    .catch((err) => reject(Error(err)));
-});
-
-const GetAll = (db) => new Promise((resolve, reject) => {
-  db('visit_schedule As vs')
-    .join('property As p', 'p.id', 'vs.property_id')
-    .join('user As u', 'u.id', 'vs.user_id')
-    .select('p.*', 'u.*', 'vs.id AS visit_id', 'time_register', 'date_register', 'status', 'vs.property_id')
-    .then((res) => resolve(res))
-    .catch((err) => reject(Error(err)));
-});
-
-const Remove = (db, property_id) => new Promise((resolve, reject) => {
-  db('visit_schedule')
-    .where({ property_id })
-    .del()
+    .select('*')
+    .where({ user_id, property_id })
     .then((res) => resolve(res))
     .catch((err) => reject(Error(err)));
 });
@@ -50,4 +51,5 @@ module.exports = {
   Get,
   GetAll,
   Remove,
+  checkEntries,
 };
